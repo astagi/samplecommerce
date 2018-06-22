d86f0fda0ab577d42246abc5c30a7b785d364dba
Andrea Stagi <stagi.andrea@gmail.com>
2018-06-22 19:30:12 +0200

    Add slug to products

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -7,7 +7,17 @@ export class Product {
     this.brief_description = brief_description
   }
 
-  name: string;
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+    this.slug = name.toLowerCase().replace(/\s/g, '_');
+  }
+
+  _name: string;
+  slug: string;
   price: number;
   description: string;
   brief_description: string;
