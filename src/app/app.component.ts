cd03deafe744d8f04ee58ef3c47080c3e7761f65
Andrea Stagi <stagi.andrea@gmail.com>
2018-06-29 21:41:02 +0200

    Init cart on startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CartCommunicationService } from './cart/cart-communication.service';
+import { CartService } from './cart/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(
+    private cartCommunicationService: CartCommunicationService,
+    private cartService: CartService,
+  ) { }
+
+  ngOnInit() {
+    this.cartService.getMyCart().subscribe(
+      cart => this.cartCommunicationService.setValue(cart)
+    )
+  }
+
 }
